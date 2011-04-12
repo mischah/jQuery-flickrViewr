5f34dd3c9cfb2ba87b05efb212e59e571c0a5b02
Michael Kühnel <mail@michael-kuehnel.de>
2011-04-12 15:48:35 +0200

    [+FEATURE] implemted render mode 'infiniteScroll'.

--- js/jquery.flickrViewr.js
+++ js/jquery.flickrViewr.js
@@ -12,18 +12,24 @@
  * --------------------------------------------------------------------
  *
  * @option apiKey string
- * @description
+ * @default ''
+ * @description 
+ * This one is required.
  * Get your API key over here: 
  * http://www.flickr.com/services/api/misc.api_keys.html
- *
+ 
  * @option photosetId string
- * @description 
- * you can get the photosetId out of a normal flickr URL 
+ * @default ''
+ * @description
+ * This one is required.
+ * You can get the photosetId out of a normal flickr URL 
  * for instance http://flickr.com/photos/mischah/sets/72157624367929792/
  * where 72157624367929792 is the photosetId
- *
+ 
  * @option imageSize string
+ * @default 'm'
  * @description 
+ * This one is optional 
  * Imagessizes provided by flickr are:  
  * 's'	-		small square 75 x 75
  * 't'	-		thumbnail, 100 on longest side
@@ -31,8 +37,36 @@
  * 'z'	-		medium 640, 640 on longest side
  * 'b'	-		large, 1024 on longest side
  * See http://www.flickr.com/services/api/misc.urls.html
-*/
+ 
+ @option render.mode string
+ * @default ''
+ * @description 
+ * This one is optional.
+ * The way you like your images to be rendered.
+ * Following rendermodes are available.
+ * 'infiniteScroll'	-	lazyload pictures while scrolling
+ *
+ * If you choose 'infiniteScroll' as renderMode you have to set a
+ * few more options …
+ 
+ @option render.infiniteScroll.perPage integer
+ * @default 500
+ * @description 
+ * This one is optional.
+ * specifies the amount of pictures to be displayed per ajax request.
+ * More than 500 are not applicable du to Flickr API restrictions.
 
+ @option render.infiniteScroll.threshold integer
+ * @default 0
+ * @description 
+ * This one is optional.
+ * This describes how close to the bottom of the page the lazyloading
+ * should be triggered. Let's say you choose a high number like 400:
+ * Now the new images are loaded 400 pixel before the user scrolled
+ * to the bottom of the page. 
+
+ 
+*/
 
 // jQuery wrapper 
 (function ($) {
@@ -62,74 +96,120 @@
 			var element = $('.flickrViewr', this);
 							
 			// Place the loader gif
-			var loader = element.append('<img src="images/jquery.flickrViewr/ajax-loader.gif" alt="Loading images …" class="flickrViewrLoader">');
-
-			var jqxhr = $.ajax({
-				url : 'http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&jsoncallback=?',
-				data : {
-					api_key: options.apiKey,
-					photoset_id: options.photosetId,
-					extras: 'date_taken,geo,tags',
-					format: 'json'
-				},
-		    	dataType : 'json',
-		    	timeout : 5000,
-		    	success : function (data) {
-					/**
-					* Errorhandling: 
-					* Check the status of the API response.
-					*/
-					if (data.stat === 'fail') {
-						// Get errormessage from API response
-						$('p', errorContainer).text(data.message+'.');
-						element.append(errorContainer);
-					}
-					else {
-						var images = '';				
-						// Loop through the results
-						$.each(data.photoset.photo, function (i, item) {
-							//build the url of the photo
-							var photoUrl =	'http://farm' + item.farm + 
-											'.static.flickr.com/' + item.server +
-											'/' + item.id +
-											'_' + item.secret + 
-											'_' + options.imageSize + 
-											'.jpg';
-							// turn the photo id into a variable
-							var photoID = item.id;
-							// put the images in a variable
-							images += '<div class="flickrViewrImage"><img src="' + photoUrl + '" alt="' + item.title + '" /></div>';
-						});
-						/*
-						* DOM manipulation:
-						* - Insert images
-						*/
-						element.append(images);
-					} 
-				}
-			});
-						
-			// If Ajax request is complete
-			jqxhr.complete(function(){
-				/*
-				* DOM manipulation:
-				* - Define container width
-				* - Delete loader
-				*/
-				$('.flickrViewrImage', element).each(function() {
-					$(this).width($('img', $(this)).outerWidth());
-				});
-				$('.flickrViewrLoader').remove();
-			});
+			var loader = $('<img src="images/jquery.flickrViewr/ajax-loader.gif" alt="Loading images …" class="flickrViewrLoader">');
+			element.append(loader);
+			
 			
 			/**
-			* Errorhandling: 
-			* - What to do if Ajax request fails
-			*/
-			jqxhr.error(function(){
-				$('p', errorContainer).text('Can’t connect do Flickr API.');
-				element.append(errorContainer);	
-			});
+			 * @param {integer} loadPage The number of the 'page' that should be loaded. Needed for lazy loading of images.
+			 * @description Self-executing function which will load page 1
+			 */
+			(function loadImages(loadPage) {
+				var jqxhr = $.ajax({
+					url: 'http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&jsoncallback=?',
+					data: {
+						api_key: options.apiKey,
+						photoset_id: options.photosetId,
+						extras: 'date_taken,geo,tags',
+						per_page: options.render.infiniteScroll.perPage,
+						page: loadPage,
+						format: 'json'
+					},
+					dataType: 'json',
+					timeout: 5000,
+					success: function (data) {
+						/**
+						 * Errorhandling: 
+						 * Check the status of the API response.
+						 */
+						if (data.stat === 'fail') {
+							// Get errormessage from API response
+							$('p', errorContainer).text(data.message + '.');
+							element.append(errorContainer);
+						}
+						else {
+							var images = '';
+							// Loop through the results
+							$.each(data.photoset.photo, function (i, item) {
+								//build the url of the photo
+								var photoUrl =	'http://farm' + item.farm + 
+												'.static.flickr.com/' + item.server +
+												'/' + item.id +
+												'_' + item.secret +
+												'_' + options.imageSize +
+												'.jpg';
+								// turn the photo id into a variable
+								var photoID = item.id;
+								// put the images in a variable
+								images += '<div class="flickrViewrImage"><img src="' + photoUrl + '" alt="' + item.title + '" /></div>';
+							});
+							/*
+							* DOM manipulation:
+							* - Insert images
+							* - store the actual page and the number of total pages on our container element
+							*/
+							element.append(images);
+							element.data({
+								page: parseInt(data.photoset.page),
+								pages: data.photoset.pages
+							})
+							
+						}
+					}
+				// If Ajax request is complete
+				}).complete(function () {
+					/*
+					* DOM manipulation:
+					* - Define container width
+					* - Delete loader
+					*/
+					$('.flickrViewrImage', element).each(function () {
+						$(this).width($('img', $(this)).outerWidth());
+					});
+					$('.flickrViewrLoader').remove();
+			
+					// Lazyloading for rendermode 'infiniteScroll'				
+					if (options.render.mode === 'infiniteScroll') {
+						
+						/**
+						* @description loading more images
+						* This function is called by the function below
+						*/ 			
+						function lazyLoad() {
+							console.log('Page'+element.data('page')+' of '+element.data('pages'));
+							console.info('lazyLoad fired');
+							if (element.data('page') < element.data('pages')) {
+								element.append(loader);
+								loadImages(element.data('page')+1);
+							}
+						}
+						
+						/**
+						* @description Check when to load more images
+						* 
+						*/ 
+						$(window).bind('scroll.flickrViewr', function () {
+							var viewportHeight = $(window).height();
+							var documentHeight = $(document).height();
+							var pixelsToTop = $(document).scrollTop();
+							// console.log('documentHeight-viewportHeight = '+(documentHeight-viewportHeight-50)+'; pixelsToTop = '+ pixelsToTop);
+							if ((documentHeight - viewportHeight - options.render.infiniteScroll.threshold) <= pixelsToTop) {
+								lazyLoad();
+								$(this).unbind('scroll.flickrViewr');
+							}
+						});
+					}
+				/**
+				 * Errorhandling: 
+				 * - What to do if Ajax request fails
+				 */
+				}).error(function () {
+					$('p', errorContainer).text('Can’t connect do Flickr API.');
+					element.append(errorContainer);
+				});
+			
+			})(1); // Initial call of loadImages(1) 
+			
 		});
 		
 
@@ -137,9 +217,17 @@
 	};
 	// Default options
 	$.fn.flickrViewr.defaults = { 
-		apiKey: '447d8ef1e0e6610a651becb938226228',
-		photosetId: '72157609003808870',
-		imageSize: 'm'
+		apiKey: '',
+		photosetId: '',
+		imageSize: 'm',
+		render: {
+			mode: '',
+			infiniteScroll : {
+				perPage : 500,
+				threshold: 0
+			} 
+		}
+	
 	};
 })(jQuery);
 
